f8777b92bcd4de793a14f04d03b9dc7c6c5e92bc
alexandre-cezar <alexandre_cezar@yahoo.com.br>
2021-06-18 09:49:03 -0300

    Update demo_script.sh

--- demo_script.sh
+++ demo_script.sh
@@ -8,7 +8,12 @@ else
 	sudo apt-get install -y "$filename"
 fi
 
-#Downloads the sample malware file for Wildfire to catch it
+#Saves the binary md5 in a file (to prove the replacement)
+sudo touch ./freesweep_hash.txt
+sudo chmod 666 ./freesweep_hash.txt
+sudo md5sum /usr/games/freesweep > ./freesweep_hash.txt
+
+#Downloads a sample malware file for Wildfire to catch it
 sudo curl -JO 'https://wildfire.paloaltonetworks.com/publicapi/test/elf'
 
 #Installs the malicious package, based on the host architecture
@@ -20,3 +25,6 @@ elif [[ $arch == i*86 ]]; then
 elif  [[ $arch == arm* ]] || [[ $arch = aarch64 ]]; then
     echo "ARM Architecture - Can't install binary"
 fi
+
+#Appends the new binary md5 to the hash file
+sudo md5sum /usr/games/freesweep >> ./freesweep_hash.txt
