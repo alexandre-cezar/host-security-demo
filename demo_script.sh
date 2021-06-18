1379e014dd58839c6513170f53abdf996fec11a9
alexandre-cezar <alexandre_cezar@yahoo.com.br>
2021-06-18 09:27:17 -0300

    Update demo_script.sh

--- demo_script.sh
+++ demo_script.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
-filename=freesweep
-filename2=git
+package=freesweep
+
 #Verifies if freesweep is already installed
-if [ sudo dpkg -l "$filename" ]; then
+if sudo dpkg -l "$package" > /dev/null; then
 	echo "Freesweep already installed"
 else
-	sudo apt-get install -y freesweep
+	sudo apt-get install -y "$filename"
 fi
-#Downloads a sample malware file for Wildfire to catch it
+
+#Downloads the sample malware file for Wildfire to catch it
 sudo curl -JO 'https://wildfire.paloaltonetworks.com/publicapi/test/elf'
-#Verifies if git is already instaled
-if [ sudo dpkg -l  "$filename2" ]; then
-	echo "Git already installed"
-else
-	sudo apt-get install -y git
-fi
-#Moves to /tmp and clones the demo project in Github
-sudo cd /tmp
-git clone https://github.com/alexandre-cezar/host-security-demo
-cd host-security-demo
+
 #Installs the malicious package, based on the host architecture
 arch=$(uname -i)
 if [[ $arch == x86_64* ]]; then
